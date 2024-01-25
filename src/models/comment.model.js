f9c616e03492d9adf4fae10e141a64acbfbb4fff
AshishDixit0 <ashishdxt121200@gmail.com>
2024-01-25 21:17:05 +0530

    add new models

---
+++ src/models/comment.model.js
@@ -0,0 +1,20 @@
+import mongoose, {Schema} from "mongoose";
+import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2";
+ const commnetSchema  = new Schema({
+    content:{
+        type: String,
+        required: true,
+    },
+    video:{
+        type: Schema.Types.ObjectId,
+        ref: "User",
+    },
+    owner:{
+        type: Schema.Types.ObjectId,
+        ref: "User",
+    }
+ },{
+    timestamps: true
+ })
+ commnetSchema.plugin(mongooseAggregatePaginate)
+ export const Comment = mongoose.model("Comment",commnetSchema)
