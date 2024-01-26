3d22ceb22082b508c24ae42e4747840c2aa7e3de
AshishDixit0 <ashishdxt121200@gmail.com>
2024-01-26 12:29:02 +0530

    add new todo controllers

--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -1,5 +1,7 @@
 import { Router } from "express";
-import {registerUser,loginUser, logoutUser, refreshAccessToken, changeCurrentPassword, getCurrentUser, updateAccountDetails, updateUserAvatar, updateUserCoverImage, getUserChannelProfile, getWatchHistory } from "../controllers/user.controller.js";
+import {registerUser,loginUser, logoutUser, refreshAccessToken, changeCurrentPassword, 
+    getCurrentUser, updateAccountDetails, updateUserAvatar, updateUserCoverImage,
+     getUserChannelProfile, getWatchHistory } from "../controllers/user.controller.js";
 import {upload} from "../middlewares/multer.middleware.js"
 import { jwtVerify } from "../middlewares/auth.middleware.js";
 const router=Router();
