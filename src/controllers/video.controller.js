da91675702142680ba25eb126e2d76795281313b
AshishDixit0 <ashishdxt121200@gmail.com>
2024-01-28 18:57:09 +0530

    vide controller

--- src/controllers/video.controller.js
+++ src/controllers/video.controller.js
@@ -53,6 +53,17 @@ const getAllVideos = asyncHandler(async (req, res) => {
 const publishAVideo = asyncHandler(async (req, res) => {
     const { title, description} = req.body
     // TODO: get video, upload to cloudinary, create video
+    const videoLocalPath = req.files?.video[0].path;
+    if(!videoLocalPath)
+    {
+        throw new ApiError(400,"does not find video path")
+    }
+    const thumbnailLocalPath= req.files?.thumbnail[0].path;
+
+    if(!thumbnailLocalPath)
+    {
+        throw new ApiError(400,"does not find video path")
+    }
     if(!title)
     {
         throw new ApiError(400,"title is missing")
@@ -61,7 +72,31 @@ const publishAVideo = asyncHandler(async (req, res) => {
     {
         throw new ApiError(400,"description is missing")
     }
-    
+    const videoFile = await uploadOnCloudinary(videoLocalPath);
+    const thumbnail= await uploadOnCloudinary(thumbnailLocalPath)
+    if(!videoFile)
+    {
+        throw new ApiError(400, "video not found")
+    }
+    if(!thumbnail)
+    {
+        throw new ApiError(400,"thumbanil not found")
+    }
+    const duration =
+          typeof videoFile.duration === "string"
+            ? parseFloat(videoFile.duration)
+            : videoFile.duration;
+
+    const newVideo = await Video.create({
+        videoFile:videoFile.url,
+        thumbnail:thumbnail.url,
+        title,
+        description,
+        owner:new mongoose.Types.ObjectId(req.user?._id),
+        duration,
+        views,
+        isPublished
+    })
 
 })
 
