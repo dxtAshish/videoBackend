c04ae3dde8b59fa631d96acacacb897e1f502604
AshishDixit0 <ashishdxt121200@gmail.com>
2024-02-05 20:36:23 +0530

    add all tweet controllers

--- src/controllers/tweet.controller.js
+++ src/controllers/tweet.controller.js
@@ -16,24 +16,45 @@ const createTweet = asyncHandler(async (req, res) => {
         content,
         owner:new mongoose.Types.ObjectId(req.user?._id),
     })
+    
+    if(!tweet) {
+        throw new ApiError(400, "Something went wrong while creating tweet")
+    }
  return res.status(200).json(new ApiResponse(200,tweet,"tweet is created successfully"));
 })
 
 const getUserTweets = asyncHandler(async (req, res) => {
     // TODO: get user tweets
-    const userId = req.user?._id;
-    if(!userId)
-    {
-        throw new ApiError(400,"user is missing");
+    const {userId} = req.params;
+    if(!isValidObjectId(userId)) {
+        throw new ApiError(400, "Invalid userId")
     }
-    const tweet=Tweet.findById(userId);
-    return res.status(200).json(new ApiResponse(200,tweet,"get user tweets"))
+    const user =User.findById(userId)
+    if(!user) {
+        throw new ApiError(400, "No user exists")
+    }
+
+    const userTweets = await Tweet.aggregate([
+        {
+            $match: {
+                owner: new mongoose.Types.ObjectId(userId)
+            }
+        }
+    ])
+
+   
+
+    if(userTweets.length === 0) {
+        throw new ApiError(400, "No tweets uploaded by user")
+    }
+
+    return res.status(200).json(new ApiResponse(200,userTweets,"get user tweets"))
 
 })
 
 const updateTweet = asyncHandler(async (req, res) => {
     //TODO: update tweet
-    const tweetId =req.tweet?._id;
+    const {tweetId} =req.params;
     if(!tweetId)
     {
         throw new ApiError(400,"tweet id is not find")
@@ -57,6 +78,32 @@ const updateTweet = asyncHandler(async (req, res) => {
 
 const deleteTweet = asyncHandler(async (req, res) => {
     //TODO: delete tweet
+    const { tweetId } = req.params
+
+    if(!isValidObjectId(tweetId)) {
+        throw new ApiError(400, "tweet id is invalid")
+    }
+
+    const tweet = await Tweet.findById(tweetId)
+
+    if(!tweet) {
+        throw new ApiError(400, "No tweet exists")
+    }
+
+    // validation
+    if(tweet?.owner.toString() !== req.user?._id.toString()) {
+        throw new ApiError(400, "Unauthorised access")
+    }
+
+    const response = await Tweet.findByIdAndDelete(tweetId)
+
+    if(!response) {
+        throw new ApiError(400, "Something went wrong while deleting tweet")
+    }
+
+    return res
+    .status(200)
+    .json(new ApiResponse(200, response, "Tweet deleted successfully"))
     
 })
 
