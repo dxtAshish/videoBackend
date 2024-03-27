52af58d413be9bc1914027a4f46d0c177a8ac5d6
dxtAshish <ashishdxt121200@gmail.com>
2024-03-27 13:23:55 +0530

    add authapi

--- FRONTEND/video-frontend/src/feature/AuthApi.jsx
+++ FRONTEND/video-frontend/src/feature/AuthApi.jsx
@@ -1,11 +1,10 @@
 import React, { useEffect } from 'react';
 import axios from 'axios';
 
-export const useRegisterUser = (data) => {
-    useEffect(() => {
-        const registerUser = async () => {
+
+export const useRegisterUser = async (data) => {
             try {
-                const response = await axios.post("/users/register", data, {
+                const response = await axios.post(`${import.meta.VITE_BACKEND_URL}users/register`, data, {
                     headers: {
                         "Content-Type": "multipart/form-data",
                     },
@@ -13,17 +12,64 @@ export const useRegisterUser = (data) => {
                 });
                
                 console.log(response.data);
+                return response.data
             } catch (error) {
              
                 console.error("Error occurred while registering user:", error);
             }
-        };
-
-        registerUser();
-
-        // Cleanup function if needed
-        return () => {
-            // cleanup code here
-        };
-    }, [data]); // Only re-run the effect if `data` changes
 };
+export const useLoginUser=async(data)=>{
+    try {
+        const response = await axios.post(`${import.meta.VITE_BACKEND_URL}users/login`, data, {
+            headers: {
+                "Content-Type": "application/json",
+            },
+            withCredentials: true,
+        });
+       
+        console.log(response.data);
+        return response.data
+    } catch (error) {
+     
+        console.error("Error occurred while registering user:", error);
+    }
+}
+export const useLogoutUser=async()=>{
+    try {
+        const response = await axios.post(`${import.meta.VITE_BACKEND_URL}users/logout`, data, {
+            headers: {
+                "Content-Type": "application/json",
+            },
+            withCredentials: true,
+        });
+       
+        console.log(response.data);
+        return response;
+    } catch (error) {
+     
+        console.error("Error occurred while registering user:", error);
+    }
+}
+export const useGetCurrentUser=async()=>{
+  try {
+    const response=await axios.get(`${import.meta.env.VITE_BACKEND_URL}users/current-user`,{
+        withCredentials:true
+    })
+    console.log(response);
+    return response.data
+  } catch (error) {
+    console.log("some error occured on fetching current user",error)
+  }
+}
+export const useGetUserChannelProfile = async (username) => {
+    try {
+        const response=await axios.get(`${import.meta.env.VITE_BACKEND_URL}users/c/${username}`,{
+            withCredentials:true
+        })
+        console.log(response);
+        return response.data;
+      } catch (error) {
+        console.log("some error occured on fetching current user",error)
+      }
+    }
+  
