2c9333f00d1cd6254139a8703496ccbe7da1e092
dxtAshish <ashishdxt121200@gmail.com>
2024-04-27 09:51:33 +0530

    user check

--- FRONTEND/video-frontend/src/component/VideoModal.jsx
+++ FRONTEND/video-frontend/src/component/VideoModal.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const VideoModal = () => {
   return (
     <div className='w-[500px] h-full bg-red-800 translate-x-1 transition'>
-      
+      <div></div>
     </div>
   )
 }
